5abff84fd7cd5b8e0a6ecb3806590566a65b6f41
aldiekaaa <120025106+aldiekaaa@users.noreply.github.com>
2023-09-13 23:37:25 +0700

    deploy first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import Header from './components/header.vue';
+  import MainSec from './components/MainSec.vue';
+  import MenuSec from './components/MenuSec.vue';
+  import Blog from './components/Blog.vue';
+
+  
+</script>
+
+<template>
+  <div class="bg-sky-400">
+    <div>
+    <Header />
+    </div>
+    <div class="p-3">
+      <MainSec />
+    </div>
+    <div class="px-3 pb-3">
+      <MenuSec />
+    </div>
+  </div>
+  <div class="mt-10">
+    <Blog />
+  </div>
+</template>
+
+<style src="./style.css" />
